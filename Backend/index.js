a2a9a340729189c3fa8a2819ff4ef1d35d58182a
Maryam <maryam.chaudhry@iths.se>
2026-07-15 23:46:10 +0200

    Added change to Vercel URL in CORS origin

--- Backend/index.js
+++ Backend/index.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 3000;
 app.use((0, cors_1.default)({
     origin: [
         "https://fullstack-project-todowebbapp.vercel.app",
-        /https:\/\/fullstack-project-todowebbapp.*\.vercel\.app/,
+        "https://fullstack-project-todowebbapp-hnipe8r4w-maryam891s-projects.vercel.app",
         "http://localhost:5173",
     ],
     credentials: true,
